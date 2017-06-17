c2ee212b3df1bfac92abd1ccaf61dd045ac358ca
Lucas Elisei <lucas.elisei@heig-vd.ch>
2017-06-17 16:19:35 +0200

    Added test users

--- sql/slo.sql
+++ sql/slo.sql
@@ -23,3 +23,7 @@ CREATE TABLE `users` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `username_UNIQUE` (`username`)
 );
+
+-- Create test users
+INSERT INTO `users` VALUES (1,'admin','546bb7a8fce3889e8cd128321cb9e9adb18b05bb2460e8a0f6305d4cbb563e1d6e754f329c111b21b84e8ba1669cc91d99d5592c6e215f2aadc8a43bb7b39ec6','6f4c6bfb7754d2f0f38a2dfb7bf8eb27',1),
+(2,'test','ef49a13e06447cae079a9f76c8374cfe4461f9dcd4f8ad9cc70e7d72a78693eefbfd0c4fe1a52938de8cd819df12f7c9ec7461435b91a82fe53a146b45b9a994','9d4afff9a2c466624ccec497644cac87',0);
